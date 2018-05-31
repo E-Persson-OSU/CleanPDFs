8af5409f85031f201d6e66006d2a57f26f9b90f7
Persson <persson.12@osu.edu>
2018-05-31 17:52:43 -0400

    Better formatting, commenting, and played a little code golf

--- CleanPDFs.py
+++ CleanPDFs.py
@@ -2,8 +2,8 @@
 
 
 #imports
-from os               import listdir, rename, remove
-from os.path          import isfile, join, getctime
+from os               import listdir, rename
+from os.path          import getctime
 from time             import localtime, strftime
 from ntpath           import basename
 from smtplib          import SMTP_SSL
@@ -13,19 +13,22 @@ from configparser     import RawConfigParser
 #config loading
 config = RawConfigParser()
 config.read('cleanpdf.ini')
-login = config['login']
-pw = config['password']
-rcp = config['recipient']
+login, pw, rcp = config['login'], config['password'], config['recipient']
 
 #begin functions
+
+#verifies file is a pdf
 def ispdf(file):
     return file.lower().endswith('.pdf')
 
+#Takes file and daily identifier, checks proper directory for file and renames it and moves it to that directory with new file name
 def renameandfile(file,di):
-    cts = getctime(file)                            # creation time in seconds
-    ctl = localtime(cts)                            # creation time local as struct_time object
-    ctf = strftime("%Y_%m_%d_%H_%M_%S", ctl)        # creation time formatted to YEAR_MONTH_DAY_HOUR_MINUTE_SECOND 
-    ctf = ctf + '_' + str(di) + '.pdf'
+
+    #name file
+    ctf = strftime("%Y_%m_%d_%H_%M_%S", localtime(getctime(file)))        # creation time formatted to YEAR_MONTH_DAY_HOUR_MINUTE_SECOND 
+    ctf = ctf + '_' + str(di) + '.pdf'                                    # Adding unique daily identifier to prevent same name files and file extension
+
+    #Determine correct folder for file
     bn = basename(file)
     folder = 'Misc\\'
     if bn.lower().startswith('ps'):
@@ -34,19 +37,24 @@ def renameandfile(file,di):
         folder = 'Return Slips\\'
     elif bn.lower().startswith('qu'):
         folder = 'Quotes\\'
-    #take action
+
+    #rename file
     rename(file, pfp + folder + ctf)
-    #log action
-    return "Successfully renamed and moved " + file + " to " + pfp + folder + ctf + "\n"
+
+    #return action string
+    return "Renamed and moved " + file + " to " + pfp + folder + ctf + "\n"
 
 
 #TODO make python file for sending emails
+
+#makes report from action list, saves it to a text document in separate folder
 def mkreport(actionlist):
     fnw = 'H:\\Scripts\\PDFCleanupReports\\' + strftime('%d_%m_%Y') + '.txt'
     with open(fnw ,'w+') as fw:
         for a in actionlist:
             fw.write(a)
         fw.write("\nEnd report.")
+    #returns file name for later use
     return fnw
 
 def sendreport(filename):
@@ -56,11 +64,11 @@ def sendreport(filename):
     
     msg['Subject'] = "PDF Cleanup on " + date
     msg['From'] = login
-    msg['To'] = rc
+    msg['To'] = rcp
 
     s = SMTP_SSL('smtp.gmail.com',465,timeout=10)
     try:
-        s.login(login,password)
+        s.login(login,pw)
         s.sendmail(msg['From'], msg['To'], msg.as_string())
     finally:
         s.quit()
@@ -69,10 +77,15 @@ def sendreport(filename):
 
 #end functions
 
-#main
-pfp = 'H:\\pdfs\\' #pdf location
+#----------BEGIN MAIN----------
+
+#PDF Folder Directory
+pfp = 'H:\\pdfs\\'
+#get files in directory
 pdflist = listdir(pfp)
+#Daily identifier equal to n, where n is index of file in pdflist
 di = 1
+#action list to record actions taken during script
 al = []
 for f in pdflist:
     fn = pfp + f
@@ -81,3 +94,4 @@ for f in pdflist:
         di += 1
 msgfile = mkreport(al)
 sendreport(msgfile)
+#-----------END MAIN-----------
